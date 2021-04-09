991cf8905f6829c47fd70f8cdd32c37e74cf0cbd
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-04-09 16:15:11 -0400

    added styled-components, started styling nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import styled from 'styled-components'
 import CitrusLogo from '../Assets/CitrusLogo.svg';
 import PhoneIcon from '@material-ui/icons/Phone';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <img src={CitrusLogo} alt={"Citrus Logo"} />
-            <div>
+        <Nav>
+            <Img src={CitrusLogo} alt={"Citrus Logo"} />
+            <Div>
                 <a><PhoneIcon />(608) 754-9006</a>
                 <div>
                     <Link to="/">Home</Link>
@@ -15,9 +16,26 @@ const Navbar = () => {
                     <Link to="/menus">Menus</Link>
                     <Link to="/about">About</Link>
                 </div>
-            </div>
-        </nav>
+            </Div>
+        </Nav>
     )
 }
 
 export default Navbar;
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    width: 95%;
+    margin: 0 auto;
+`
+
+const Img = styled.img`
+    height: 100px;
+`
+
+const Div = styled.div`
+    margin: 0 50px 0 0;
+    
+`
+
