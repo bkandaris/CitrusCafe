ff50b2b3d0159b657643373a7855412567a105b6
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-04-09 22:12:51 -0400

    finished most nav styling, need to add fonts still

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,15 +7,17 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <Nav>
-            <Img src={CitrusLogo} alt={"Citrus Logo"} />
+            <Link to="/">
+                <Img src={CitrusLogo} alt={"Citrus Logo"} />
+            </Link>
             <Div>
-                <a><PhoneIcon />(608) 754-9006</a>
-                <div>
-                    <Link to="/">Home</Link>
-                    <Link to="/contact" >Contact</Link>
-                    <Link to="/menus">Menus</Link>
-                    <Link to="/about">About</Link>
-                </div>
+                <A href="tel:6087549006"><PhoneIcon fontSize={'inherit'} className="phone_icon" />(608) 754-9006</A>
+                <LinksDiv>
+                    <Link className="nav_link" to="/">Home</Link>
+                    <Link className="nav_link" to="/contact" >Contact</Link>
+                    <Link className="nav_link" to="/menus">Menus</Link>
+                    <Link className="nav_link" to="/about">About</Link>
+                </LinksDiv>
             </Div>
         </Nav>
     )
@@ -26,16 +28,39 @@ export default Navbar;
 const Nav = styled.nav`
     display: flex;
     justify-content: space-between;
-    width: 95%;
+    width: 85%;
     margin: 0 auto;
+
 `
 
 const Img = styled.img`
-    height: 100px;
+    height: 80px;
 `
 
 const Div = styled.div`
     margin: 0 50px 0 0;
-    
+    width: 255px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    margin-bottom: 20px;
+`
+
+const A = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: orange;
+    color: white;
+    border-radius: 0px 0px 10px 10px;
+    font-weight: 600;
+    padding: .2em;
+    cursor: pointer;
+`
+
+const LinksDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    font-weight: 400;
 `
 
